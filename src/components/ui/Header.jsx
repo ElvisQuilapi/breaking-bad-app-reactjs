416a25fdb08f0cc73e521a8bedeaababc5b58b95
-api <elvisquilapi@gmail.com>
2020-07-07 18:01:07 -0400

    code 1

---
+++ src/components/ui/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Logo from '../../assets/logo.png'
+const Header = () => {
+    return (
+        <header className="center">
+            <img src={Logo} />
+        </header>
+    )
+}
+
+export default Header
